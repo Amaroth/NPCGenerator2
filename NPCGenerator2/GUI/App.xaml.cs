4b8963a4fe475512e7aacb6da96dc70fa1c0f2a3
Amaroth <Amaroth7@seznam.cz>
2018-02-24 16:47:16 +0100

    Initial VS project commit.

---
+++ NPCGenerator2/GUI/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace NPCGenerator2.GUI
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var mwd = new MainWindow();
+            mwd.LoadSettings();
+            mwd.Show();
+        }
+    }
+}
---
+++ NPCGenerator2/GUI/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace NPCGenerator2.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void LoadSettings()
+        {
+            
+        }
+    }
+}
